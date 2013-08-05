025f6a3d724085e5329d767d5353732cd10fbfdb
Henric Trotzig <henric.trotzig@gmail.com>
2013-08-04 18:15:35 -0700

    Add uniqueness validation to address

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,7 @@
 class Url < ActiveRecord::Base
   attr_accessible :address
   validates_format_of :address, :with => /https?:\/\/.+/
+  validates_uniqueness_of :address
+
   has_many :snapshots
 end
