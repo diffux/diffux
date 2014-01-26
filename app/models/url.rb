1a784c3eab5b6acb7a9e785486fc975d721be2c8
Henric Trotzig <henric.trotzig@gmail.com>
2014-01-26 15:57:24 -0800

    Align code in Url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,9 +1,13 @@
 class Url < ActiveRecord::Base
-  validates_presence_of :viewport_width, :address, :name
-  validates_format_of :address, :with => /https?:\/\/.+/
+  validates_presence_of :viewport_width,
+                        :address,
+                        :name
+
+  validates_format_of     :address, :with => /https?:\/\/.+/
   validates_uniqueness_of :address
 
   has_many :snapshots
   has_one  :baseline
+
   default_scope order(:name)
 end
