f55fb3e0f8ccf70eb145f1dfae39e945bd89a2e3
Henric Trotzig <henric.trotzig@gmail.com>
2014-01-27 13:48:15 -0800

    Use `secret_token` instead of `secret_key_base`
    `secret_key_base` is new in Rails 4, and replaces `secret_token`. We
    don't need to be backwards-compatible, so I removed `secret_token`
    altogether.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Diffux::Application.config.secret_token = 'e17baca830a062643393dc8df8179a46929db0ac82f7cc92fa789273c18e35791bb35d7056bb6ef99a0dbe1262b0047fc0c7b3085146161e8929307ff3492c7c'
+Diffux::Application.config.secret_key_base = 'e17baca830a062643393dc8df8179a4692'
