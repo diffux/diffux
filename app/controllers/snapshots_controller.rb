27bcd51ce9cd2e8b6784020c093eedf97b4c4ba1
Joe Lencioni <joe.lencioni@causes.com>
2014-01-27 20:01:38 -0800

    Simplify SnapshotsController
    In 1972a90c2bf09ecb3b8d7 we removed the JSON-response support that Rails
    auto-generated as part of initializing the Url model. We didn't need it
    there, and we don't need it here, in SnapshotsController.
    
    Change-Id: I2af4d2c97cbeb1b1f0559dc1915c4ff96ee0d1b7

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -1,6 +1,4 @@
 class SnapshotsController < ApplicationController
-  # GET /snapshots
-  # GET /snapshots.json
   def index
     if params[:url]
       @url = Url.find(params[:url])
@@ -8,48 +6,26 @@ class SnapshotsController < ApplicationController
     else
       @snapshots = Snapshot.all
     end
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @snapshots }
-    end
   end
 
-  # GET /snapshots/1
-  # GET /snapshots/1.json
   def show
     @snapshot = Snapshot.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @snapshot }
-    end
   end
 
-  # POST /snapshots
-  # POST /snapshots.json
   def create
     url = Url.find(params.delete(:url))
     @snapshot = Snapshot.new
     @snapshot.url = url
 
-    respond_to do |format|
-      @snapshot.save!
-      format.html { redirect_to url, notice: 'Snapshot was successfully created.' }
-      format.json { render json: @snapshot, status: :created, location: @snapshot }
-    end
+    @snapshot.save!
+    redirect_to url, notice: 'Snapshot was successfully created.'
   end
 
-  # DELETE /snapshots/1
-  # DELETE /snapshots/1.json
   def destroy
     @snapshot = Snapshot.find(params[:id])
     @snapshot.destroy
 
-    respond_to do |format|
-      format.html { redirect_to @snapshot.url }
-      format.json { head :no_content }
-    end
+    redirect_to @snapshot.url
   end
 
   def set_as_baseline
