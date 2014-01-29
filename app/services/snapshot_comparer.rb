8339ff0a9a82d2e083959de198a5a215f87ac20b
Joe Lencioni <joe.lencioni@causes.com>
2014-01-29 08:13:34 -0800

    Rename image_after, image_before -> png_after, png_before
    More specifically than just images, these are special ChunkyPNG objects.
    Changing the name may make this more clear, and it results in shorter
    lines.
    
    Change-Id: I5ba2f554f3d90ad6f1a56dc5c6c46002d710cc09

--- app/services/snapshot_comparer.rb
+++ app/services/snapshot_comparer.rb
@@ -11,18 +11,18 @@ class SnapshotComparer
 
   # @see http://jeffkreeftmeijer.com/2011/comparing-images-and-creating-image-diffs/
   def compare!
-    image_after  = to_chunky_png(@snapshot_after)
-    image_before = to_chunky_png(@snapshot_before)
+    png_after  = to_chunky_png(@snapshot_after)
+    png_before = to_chunky_png(@snapshot_before)
 
-    output = ChunkyPNG::Image.new([image_before.width, image_after.width].max,
-                                  [image_before.height, image_after.height].max,
+    output = ChunkyPNG::Image.new([png_before.width, png_after.width].max,
+                                  [png_before.height, png_after.height].max,
                                   WHITE)
     diff = 0
-    min_width = [image_before.width, image_after.width].min
-    image_before.height.times do |y|
-      image_before.row(y).each_with_index do |pixel, x|
-        if x < min_width && (pixel != image_after[x, y])
-          score        = pixel_diff_score(image_after[x, y], pixel)
+    min_width = [png_before.width, png_after.width].min
+    png_before.height.times do |y|
+      png_before.row(y).each_with_index do |pixel, x|
+        if x < min_width && (pixel != png_after[x, y])
+          score        = pixel_diff_score(png_after[x, y], pixel)
           output[x, y] = grayscale(MAX - (score * MAX).round)
           diff        += score
         end
@@ -30,7 +30,7 @@ class SnapshotComparer
     end
 
     result = {
-      diff_in_percent: diff.to_f / image_before.pixels.length * 100
+      diff_in_percent: diff.to_f / png_before.pixels.length * 100
     }
     FileUtil.with_tempfile do |file|
       output.save(file)
