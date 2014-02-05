daf9fc99790dfa3aa225837fa2aff440e23538c0
Joe Lencioni <joe.lencioni@causes.com>
2014-02-05 14:28:52 -0800

    Add Snapshotter spec
    This object was entirely untested, leaving us vulnerable to regressions.
    This commit fills in the gaps with a few simple tests.
    
    Change-Id: I476730f70b798eddca86416285f32ca77c6b947e

---
+++ spec/services/snapshotter_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+require 'json'
+
+describe Snapshotter do
+  let(:url)      { create :url }
+  let(:viewport) { create :viewport }
+  subject        { Snapshotter.new(url, viewport) }
+
+  describe '#take_snapshot!' do
+    let(:external_image_id) { rand(1_000) }
+
+    before do
+      Phantomjs.stubs(:run).yields(encoded_output)
+      FileUtil.stubs(:upload_to_cloudinary).returns(external_image_id)
+    end
+
+    context 'when snapshot script outputs JSON' do
+      let(:decoded_output) { { something: rand(1_000).to_s } }
+      let(:encoded_output) { ActiveSupport::JSON.encode(decoded_output) }
+
+      it 'returns the decoded JSON' do
+        response = subject.take_snapshot!
+        (decoded_output.to_a - response.to_a).should == []
+      end
+
+      it 'uploads the file to Cloudinary' do
+        FileUtil.expects(:upload_to_cloudinary)
+        subject.take_snapshot!
+      end
+
+      it 'includes the external image ID' do
+        subject.take_snapshot![:external_image_id].should == external_image_id
+      end
+    end
+
+    context 'when snapshot script outputs non-JSON' do
+      let(:encoded_output) { 'This is not JSON' }
+      it 'raises an error with the script output' do
+        expect { subject.take_snapshot! }.to raise_error(encoded_output)
+      end
+    end
+  end
+end
