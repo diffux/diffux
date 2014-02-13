diffux/diffux

commit e8d2199ea2305d573e97550a04c68d513d07f75f

Add email to sweeps, and ping when ready
This commit adds an email field to sweeps. If you add an email address,
it will be pinged when the sweep is ready (no pending snapshots). The
email is only sent once, and contains a link to the sweep page for easy
review.

The reasoning behind all this is that taking snapshots can take some
time, and the asyncness of the process might make it so that you forget
to follow up and review your sweep.

Some details:

  - I've added a very simple model validation to email addresses that
    only checks for an "@"-sign. Let's keep it this simple, I've seen
    too many email regexps that try to be too smart, and they almost
    always fail for certain types of email addresses.

  - I've made the snapshot model know less about the sweep by simply
    calling `refresh!` on create/update (instead of `update_counters!`).

  - The production email config is not functional yet, we need to
    re-visit this soon.

  - The development env will "send" emails to the `tmp/emails` folder,
    and save each email in a file. This makes testing in development
    easier.

  - Fixed a small bug with how the SweepWorker was run when the sweep
    didn't need to be delayed. Now a worker thread will only be created
    if delay_seconds is > 0 (which was the intention from the start).

  - I only created a text version of the email so far. I don't think we
    need to get fancy about this email. I worry a bit about the URL, I
    don't really know if it will be auto-linked in email clients. If
    not, we should probably create a simple html version instead.

app/models/sweep.rb
@@ -3,6 +3,7 @@ class Sweep < ActiveRecord::Base
   has_many              :snapshots
   attr_accessor         :delay_seconds
   validates_presence_of :title
+  validates_format_of   :email, with: /\A.+@.+\Z/, allow_nil: true
   after_create          :take_snapshots
   before_create         :set_start_time_from_delay_seconds
 
@@ -30,6 +31,13 @@ class Sweep < ActiveRecord::Base
     snapshots.select(&:under_review?)
   end
 
+  def refresh!
+    update_counters!
+    send_email_if_needed!
+  end
+
+  private
+
   def update_counters!
     self.count_pending      = pending_snapshots.count
     self.count_accepted     = accepted_snapshots.count
@@ -38,7 +46,16 @@ class Sweep < ActiveRecord::Base
     save!
   end
 
-  private
+  def send_email_if_needed!
+    return unless email
+    update_counters! # to prevent stale data
+    return if snapshots.count == 0
+    return if count_pending > 0
+    return if emailed_at
+    SweepMailer.ready_for_review(self).deliver
+    self.emailed_at = Time.now
+    save!
+  end
 
   def take_snapshots
     if start_time
@@ -49,7 +66,7 @@ class Sweep < ActiveRecord::Base
   end
 
   def set_start_time_from_delay_seconds
-    return unless delay_seconds
+    return unless delay_seconds.to_i > 0
     self.start_time = Time.now + delay_seconds.to_i.seconds
   end
 end

spec/controllers/sweeps_controller_spec.rb
@@ -87,6 +87,7 @@ describe SweepsController do
     let(:project)     { create(:project, :with_viewport, :with_url) }
     let(:title)       { Random.rand(10..100).to_s }
     let(:description) { Random.rand(100..1000).to_s }
+    let(:email)       { 'foo@bar.com' }
 
     let(:params) do
       {
@@ -94,6 +95,7 @@ describe SweepsController do
         sweep: {
           title:       title,
           description: description,
+          email:       email,
         }
       }
     end
@@ -119,7 +121,7 @@ describe SweepsController do
 
       context 'with a delay' do
         before do
-          params[:sweep][:delay_seconds] = 60
+          params[:sweep][:delay_seconds] = '60'
         end
 
         it { should be_redirect }
@@ -141,12 +143,42 @@ describe SweepsController do
           expect { subject }.to change { SweepWorker.jobs.size }.by(1)
         end
       end
+
+      context 'with an empty delay' do
+        before do
+          params[:sweep][:delay_seconds] = ''
+        end
+
+        it 'adds a sweep' do
+          expect { subject }.to change { Sweep.count }.by(1)
+        end
+
+        it 'does not set a future start_time to the sweep' do
+          subject
+          Sweep.last.start_time.should be_nil
+        end
+      end
     end
 
     context 'with a missing title' do
       let(:title) { nil }
 
-      it { should be_succes }
+      it { should be_success }
+      it { should render_template('sweeps/new') }
+
+      it 'does not add a snapshot' do
+        expect { subject }.to_not change { Snapshot.count }
+      end
+
+      it 'does not add a sweep' do
+        expect { subject }.to_not change { Sweep.count }
+      end
+    end
+
+    context 'with an invalid email address' do
+      let(:email) { 'foo' }
+
+      it { should be_success }
       it { should render_template('sweeps/new') }
 
       it 'does not add a snapshot' do

app/controllers/sweeps_controller.rb
@@ -42,6 +42,9 @@ class SweepsController < ApplicationController
   end
 
   def sweep_params
-    params.require(:sweep).permit(:title, :description, :delay_seconds)
+    params.require(:sweep).permit(:title,
+                                  :description,
+                                  :delay_seconds,
+                                  :email)
   end
 end

app/mailers/sweep_mailer.rb
@@ -1,0 +1,8 @@
+class SweepMailer < ActionMailer::Base
+  default from: 'no-reply@diffux'
+
+  def ready_for_review(sweep)
+    @sweep = sweep
+    mail(to: sweep.email, subject: "Re: #{sweep.title}")
+  end
+end

app/models/snapshot.rb
@@ -21,7 +21,7 @@ class Snapshot < ActiveRecord::Base
   before_save  :auto_accept
   after_commit :take_snapshot, on: :create
   after_commit :compare_snapshot_if_needed, on: :update
-  after_commit :update_sweep_counters, on: [:create, :update]
+  after_commit :refresh_sweep, on: [:create, :update]
 
   def diff?
     diffed_with_snapshot_id? && diffed_with_snapshot_id != id
@@ -82,9 +82,9 @@ class Snapshot < ActiveRecord::Base
     SnapshotComparerWorker.perform_async(id) if compare?
   end
 
-  def update_sweep_counters
+  def refresh_sweep
     return unless sweep
-    sweep.update_counters!
+    sweep.refresh!
   end
 
   def waiting_for_diff?

app/views/sweep_mailer/ready_for_review.text.haml
@@ -1,0 +1,7 @@
+Your Diffux sweep is ready.
+
+= project_sweep_url(@sweep, project_id: @sweep.project)
+
+Result summary:
+\- #{pluralize(@sweep.count_accepted, 'auto-accepted snapshot')}
+\- #{pluralize(@sweep.count_under_review, 'snapshot')} to review

app/views/sweeps/_form.html.haml
@@ -9,10 +9,16 @@
     = f.label :description, 'Description', class: 'control-label'
     = f.text_area :description, rows: 4, class: 'form-control'
   .form-group
+    = f.label :email, 'E-mail', class: 'control-label'
+    = f.text_field :email, class: 'form-control', type: 'email'
+    %p.help-block
+      If you specify an email address here, a message will be sent to it when
+      all snapshots are ready.
+  .form-group
     = f.label :delay_seconds, 'Delay snapshotting (seconds)', class: 'control-label'
     = f.text_field :delay_seconds, class: 'form-control', type: 'number',
       min: '1', step: '1', pattern: '\d+'
-    %span.help-block
+    %p.help-block
       Enter the number of seconds you wish to delay the start of snapshot
       creation with. Leave empty to start snapshotting immediately.
   .form-group

config/environments/development.rb
@@ -13,6 +13,13 @@ Diffux::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  # Save emails to file under tmp/emails
+  config.action_mailer.delivery_method = :file
+  config.action_mailer.file_settings  = {
+    location: Rails.root.join('tmp', 'emails')
+  }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 

commit d57b3bdd50eaae98bea655fc7f3797af3ab8e69f

Fix bug with blank sweep address
I introduced a bug in commit e8d2199ea2305 - if the email field was left
blank when trying to create a sweep, the validation would fail (even
though it was meant to be optional).

app/models/sweep.rb
@@ -3,7 +3,9 @@ class Sweep < ActiveRecord::Base
   has_many              :snapshots
   attr_accessor         :delay_seconds
   validates_presence_of :title
-  validates_format_of   :email, with: /\A.+@.+\Z/, allow_nil: true
+  validates_format_of   :email, with: /\A.+@.+\Z/,
+                                allow_nil:   true,
+                                allow_blank: true
   after_create          :take_snapshots
   before_create         :set_start_time_from_delay_seconds
 

spec/controllers/sweeps_controller_spec.rb
@@ -189,5 +189,13 @@ describe SweepsController do
         expect { subject }.to_not change { Sweep.count }
       end
     end
+
+    context 'with an empty email address' do
+      let(:email) { '' }
+
+      it 'adds a sweep' do
+        expect { subject }.to change { Sweep.count }.by(1)
+      end
+    end
   end
 end