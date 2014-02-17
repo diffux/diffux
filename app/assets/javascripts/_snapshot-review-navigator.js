595e54774281140264ced8e534d22db92562b8b1
Henric Trotzig <henric.trotzig@gmail.com>
2014-02-16 16:25:08 -0800

    Auto-scroll to active snapshot in review panel
    Now that the review panel has side-scroll, we want to make sure that the
    active one is displayed on "page load" (since we're using turbolinks,
    this concept is kind of vague).

--- app/assets/javascripts/_snapshot-review-navigator.js
+++ app/assets/javascripts/_snapshot-review-navigator.js
@@ -1,4 +1,15 @@
 $(function() {
+
+  $(document).on('page:change', function() {
+    var $activeSnapshot = $('.snapshot-card-active');
+    if (!$activeSnapshot.length) {
+      return;
+    }
+    var $panelBody = $activeSnapshot.closest('.panel-body'),
+        scrollLeft = $activeSnapshot.offset().left - $panelBody.width() / 3;
+    $panelBody.scrollLeft(scrollLeft);
+  });
+
   $(document).on('keyup', function(event) {
     var $panel          = $('.snapshot-review-panel'),
         $activeSnapshot = $panel.find('.snapshot-card-active'),
