90a550ef23a8b90c497040999d82dd46a97a702b
Joe Lencioni <joe.lencioni@causes.com>
2014-02-18 21:42:42 -0800

    Disable CSS transitions, jQuery animations when snapshotting
    Snapshots may end up being taken at slightly different points in the
    animation, causing a visual difference when there really is no
    difference. This has the potential to cause a lot of noise.
    
    Disabling CSS transitions should prevent a lot of animations from
    happening on the page.
    
    Since many sites rely on jQuery for animations, we can reduce the amount
    of noise caused by this problem by disabling jQuery animations when the
    page loads, if jQuery is available. We can do this by setting $.fx.off
    to true. More info:
    
      http://api.jquery.com/jQuery.fx.off/
    
    There may be other things that we can do to reduce the noise caused by
    animations, but this seems like a pretty good start considering how many
    sites rely on a combination of CSS and jQuery to do animations.
    
    Change-Id: Ic9e3b9285b67fd1309984ee2d1ac4aea3d947e8e

--- script/take-snapshot.js
+++ script/take-snapshot.js
@@ -4,8 +4,27 @@ var page = require('webpage').create(),
 page.viewportSize = opts.viewportSize;
 page.open(opts.address, function(status) {
   setTimeout(function() {
+    // Try to prevent animations from running, to reduce variation in
+    // snapshots.
+    page.evaluate(function() {
+      // CSS Transitions
+      var css   = document.createElement('style');
+      css.type  = 'text/css';
+      document.head.appendChild(css);
+      var sheet = css.sheet;
+      sheet.addRule('*', '-webkit-transition: none !important;');
+      sheet.addRule('*', 'transition: none !important;');
+
+      // jQuery
+      if (typeof $ !== 'undefined' && typeof $.fx !== 'undefined') {
+        $.fx.off = true;
+      }
+    });
+
+    // Save a PNG of the rendered page
     page.render(opts.outfile);
 
+    // Capture metadata
     var response = page.evaluate(function() {
       return { title: document.title };
     });
