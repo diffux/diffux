a7637a0b13f62a52f1cbe23a0558cb1766bd4553
Joe Lencioni <joe.lencioni@causes.com>
2014-02-23 14:04:50 -0800

    Replace snapshot keyboard navigation with more general solution
    In 655263bf12837ef I added code that allows us to use
    data-keyboard-focusable attributes to specify which elements on a page
    are focusable via keyboard shortcuts j and k, and openable via keyboard
    shortcuts o and Enter. This commit completes some of the cleanup
    promised there by partially replacing the system we were using for
    navigation between snapshots within a sweep with this new, more general
    system. To finish this work, I'll be adding keyboard shortcuts for [ and
    ] which perform a j or k, respectively, immediately followed by an o
    keyboard shortcut.
    
    Change-Id: Ia7db2e2beedaef6773c032d46c2c09f948ca263c

--- app/assets/javascripts/_keyboard-shortcuts.js
+++ app/assets/javascripts/_keyboard-shortcuts.js
@@ -1,16 +1,4 @@
 $(function() {
-  $(document).on('keyup', function(event) {
-    if ($(event.target).is(':input')) {
-      return;
-    }
-
-    var $el = $('[data-keyboard-shortcut=' + event.which + ']'),
-        href = $el.attr('href');
-    if ($el.length && href) {
-      Turbolinks.visit(href);
-    }
-  });
-
   $(document).on('keypress', function(event) {
     if ($(event.target).is(':input')) {
       return;
