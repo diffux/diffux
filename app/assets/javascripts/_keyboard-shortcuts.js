655263bf12837ef71eedf2ddbe3f3bd7cad1e278
Joe Lencioni <joe.lencioni@causes.com>
2014-02-23 13:48:47 -0800

    Add j, k, o, and Enter keyboard shortcuts
    We want Diffux to be easy for power users to use. For us, this means
    convenient keyboard shortcuts are a must. To that end, this commit sets
    up an easy to deploy mechanism for using j, k, o, and Enter keyboard
    shortcuts to navigate between focusable items on the page.
    
    In short, j and k will focus the next or previous focusable element. o
    or Enter will click it if it is a link, or will click the first
    descendant link within the focused element. To specify which elements
    are focusable, simply add the data-keyboard-focusable attribute to the
    element. The currently focused element is given a .keyboard-focused
    class.
    
    This commit deploys this to the project listing page and only includes
    styling for focused table rows, but I will soon follow up with some
    other places and clean out my previous attempt at keyboard shortcuts.
    
    This is in service of #13.
    
    Change-Id: I4103c7f27faf5098420782d6872bfe173eb9482c

--- app/assets/javascripts/_keyboard-shortcuts.js
+++ app/assets/javascripts/_keyboard-shortcuts.js
@@ -10,4 +10,64 @@ $(function() {
       Turbolinks.visit(href);
     }
   });
+
+  $(document).on('keypress', function(event) {
+    if ($(event.target).is(':input')) {
+      return;
+    }
+
+    switch (event.which) {
+      case 106: // j
+        focusNextFocusable();
+        event.preventDefault();
+        break;
+
+      case 107: // k
+        focusPreviousFocusable();
+        event.preventDefault();
+        break;
+
+      case 13:  // Enter
+      case 111: // o
+        openFocused();
+        event.preventDefault();
+        break;
+    }
+
+    function focusNextFocusable() {
+      if (!moveFocus(1)) {
+        $('[data-keyboard-focusable]:first:visible')
+          .addClass('keyboard-focused');
+      }
+    }
+
+    function focusPreviousFocusable() {
+      moveFocus(-1);
+    }
+
+    // @param movement [Integer] -1 to move backwards 1, or 1 to move forward 1
+    // @return [Boolean] true if movement was successful, false otherwise
+    function moveFocus(movement) {
+      var $focusable = $('[data-keyboard-focusable]:visible'),
+          $focused   = $focusable.filter('.keyboard-focused');
+      if ($focused.length) {
+        var moveTo = $focusable.index($focused) + movement;
+        if (moveTo >= 0 && moveTo < $focusable.length) {
+          $focused.removeClass('keyboard-focused');
+          $focusable.eq(moveTo).addClass('keyboard-focused');
+        }
+        return true;
+      }
+      return false;
+    }
+
+    function openFocused() {
+      var $focused = $('[data-keyboard-focusable]:visible.keyboard-focused');
+      if ($focused.is('a')) {
+        $focused[0].click();
+      } else {
+        $focused.find('a:visible:first')[0].click();
+      }
+    }
+  });
 });
