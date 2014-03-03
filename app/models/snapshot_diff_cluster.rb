1474692316688b39135d894f05deba2cbd7eb0dc
Henric Trotzig <henric.trotzig@gmail.com>
2014-03-02 20:10:50 -0800

    Move some ruby code out of snapshot `#show` template
    I added some percentage calculation to the
    app/views/snapshots/show.html.haml template in a recent commit. That
    code belongs in a ruby file so I moved it to the model. The model is
    still thin, so even if the new methods could be considered helpers I
    think it's fine to keep them here for a while.

--- app/models/snapshot_diff_cluster.rb
+++ app/models/snapshot_diff_cluster.rb
@@ -5,4 +5,15 @@ class SnapshotDiffCluster < ActiveRecord::Base
                             :finish
 
   default_scope { order(:start) }
+
+  # @return [Float] start expressed in percent relative to an image_height
+  def relative_start(image_height)
+    start.to_f / image_height * 100
+  end
+
+  # @return [Float] the cluster height expressed in percent relative to an image
+  #   height
+  def relative_height(image_height)
+    (finish + 1 - start).to_f / image_height * 100
+  end
 end
