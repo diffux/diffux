92b898240fb43b2c30bedad287b25b6791518eb5
Joe Lencioni <joe.lencioni@causes.com>
2014-03-05 17:42:24 -0800

    Fix capitalization of return descriptions in Viewport
    Apparently YARD turns this into a sentence that begins with the word
    "Returns", so having it begin with a lowercase letter is preferred here.
    
    Change-Id: I2dd9a114b2ba41b5db07700ab5f0105e3f2a7bd8

--- app/models/viewport.rb
+++ app/models/viewport.rb
@@ -6,13 +6,13 @@ class Viewport < ActiveRecord::Base
   belongs_to :project
   validates :project, presence: true
 
-  # @return [String] A representation of the Viewport instance in the format of
+  # @return [String] a representation of the Viewport instance in the format of
   #   {width}x{height}.
   def to_s
     [width, height].join('x')
   end
 
-  # @return [Integer] Dynamically calculated height based on the width. For
+  # @return [Integer] dynamically calculated height based on the width. For
   #   narrow widths, this will be tall, as if in portrait mode on a phone. For
   #   wider widths, this will be shorter, as if in landscape orientation on a
   #   laptop or desktop monitor.
