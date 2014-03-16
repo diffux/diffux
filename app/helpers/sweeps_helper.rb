04f48e0a9e8d8cce0be55d2a0b598d4ea76c80a5
Henric Trotzig <henric.trotzig@gmail.com>
2014-03-16 10:42:12 -0700

    Introduce stacked progress bar for sweeps
    To be able to quickly see the status for a sweep, I'm introducing a
    progress bar that displays the progress. The progress bar is stacked in
    the following order:
    
    1. Under review
    2. Accepted
    3. Rejected
    
    I choose to keep the old `sweep_status` method and use it to display
    a readable status format when you hover over a progress bar.
    
    The progress bar is displayed in the following places:
    
    - On the project page, under "Last sweep"
    - On the sweeps#index page, in the table
    - On the sweeps#show page

--- app/helpers/sweeps_helper.rb
+++ app/helpers/sweeps_helper.rb
@@ -1,5 +1,14 @@
 # Helper methods related to sweeps.
 module SweepsHelper
+  PROGRESS_BAR_STYLE_MAPPINGS = {
+    'pending'      => nil,
+    'under_review' => 'progress-bar-warning',
+    'accepted'     => 'progress-bar-success',
+    'rejected'     => 'progress-bar-danger',
+  }
+
+  # @param  [Sweep]  sweep
+  # @return [String] a short textual representation of the status of a sweep
   def sweep_status(sweep)
     if sweep.count_pending > 0
       pluralize(sweep.count_pending, 'pending snapshot')
@@ -12,6 +21,27 @@ module SweepsHelper
       'All accepted'
     else
       'Unknown'
+    end
+  end
+
+  # @param  [Sweep]  sweep
+  # @return [String] a stacked <div class="progress"/> Bootstrap element.
+  # @see http://getbootstrap.com/components/#progress
+  def sweep_progress_bar(sweep)
+    total_count = PROGRESS_BAR_STYLE_MAPPINGS.keys.inject(0) do |sum, state|
+      sum + sweep.send("count_#{state}")
+    end
+
+    content_tag(:div, class: 'progress', title: sweep_status(sweep)) do
+      PROGRESS_BAR_STYLE_MAPPINGS.map do |state, bootstrap_class|
+        next unless bootstrap_class
+        percent = if total_count > 0
+                    number_to_percentage(
+                      sweep.send("count_#{state}") / total_count.to_f * 100)
+                  else 0 end
+        content_tag(:div, nil, class: "progress-bar #{bootstrap_class}",
+                               style: "width: #{percent}")
+      end.join.html_safe
     end
   end
 end
