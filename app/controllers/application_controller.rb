7b5cc3861bea80242d88d5056cc161a0e4e8af7e
Joe Lencioni <joe.lencioni@causes.com>
2014-03-22 20:20:12 -0700

    Add basic i18n
    For now I added a check for a query string parameter to set the locale.
    To change the locale, just use the `locale` param.
    
    Since we don't have any concept of an account or a person's preferences,
    we may want to check the Accept-Language HTTP header to determine which
    language to use. More info on using the Accept-Language HTTP header can
    be found at:
    
      http://guides.rubyonrails.org/i18n.html#setting-the-locale-from-the-client-supplied-information
    
    For now, this is just a quick first pass at extracting copy into an
    English configuration file. Next steps include extracting more copy and
    adding additional translations.
    
    Addresses #35
    
    Change-Id: I314331dd373dfb6309f50633dd98320b040b8b96

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # Base controller for the Diffux app.
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
