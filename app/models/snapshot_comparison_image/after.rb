b8eaf01a449025b4d3d57379257ccbb49582807e
Joe Lencioni <joe.lencioni@causes.com>
2014-03-23 14:27:02 -0700

    Add color overlay to before and after comparison images
    These color overlays help you understand what happened with each image.
    Red represents removed and green represents added.
    
    Change-Id: Iadc75852ccaf989e71879dd59c805bbc6d67f26f

--- app/models/snapshot_comparison_image/after.rb
+++ app/models/snapshot_comparison_image/after.rb
@@ -1,6 +1,8 @@
 # This subclass of `SnapshotComparisonImage` knows how to draw the
 # representation of the "after" image.
 class SnapshotComparisonImage::After < SnapshotComparisonImage
+  GREEN = 2_241_396_991 # #859900
+
   # @param y [Integer]
   # @param row [Diff::LCS:ContextChange]
   def render_changed_row(y, row)
@@ -11,7 +13,7 @@ class SnapshotComparisonImage::After < SnapshotComparisonImage
   # @param row [Diff::LCS:ContextChange]
   def render_added_row(y, row)
     row.new_element.each_with_index do |pixel_after, x|
-      @output.set_pixel(x, y, pixel_after)
+      render_colored_pixel(pixel_after, GREEN, x, y)
     end
   end
 end
--- app/models/snapshot_comparison_image/before.rb
+++ app/models/snapshot_comparison_image/before.rb
@@ -1,6 +1,8 @@
 # This subclass of `SnapshotComparisonImage` knows how to draw the
 # representation of the "before" image.
 class SnapshotComparisonImage::Before < SnapshotComparisonImage
+  RED = 3_694_276_607 # #dc322f
+
   # @param y [Integer]
   # @param row [Diff::LCS:ContextChange]
   def render_changed_row(y, row)
@@ -11,7 +13,7 @@ class SnapshotComparisonImage::Before < SnapshotComparisonImage
   # @param row [Diff::LCS:ContextChange]
   def render_deleted_row(y, row)
     row.old_element.each_with_index do |pixel_before, x|
-      @output.set_pixel(x, y, pixel_before)
+      render_colored_pixel(pixel_before, RED, x, y)
     end
   end
 end
