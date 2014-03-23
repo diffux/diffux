50a447f777687809d9622dbad633775e75708109
Joe Lencioni <joe.lencioni@causes.com>
2014-03-23 11:56:08 -0700

    Order projects list by name
    To prevent these from moving around willy-nilly and to improve the
    findability of data, I am alphabetizing the list of projects on the
    ProjectsController#index page.
    
    Change-Id: I2164558e236de843198c12a782a64f44cf20e78a

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
   before_filter :set_project, only: %i[show edit update destroy]
 
   def index
-    @projects = Project.all.includes(:viewports)
+    @projects = Project.order(:name).includes(:viewports)
   end
 
   def show
